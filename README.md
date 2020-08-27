c2c5e94abe5a2d317e4b7f01e7ac74d17eb73769
wangxu <wangxu13@kingsoft.com>
2020-08-27 15:31:08 +0800

    add: add join_file (while provide join_file ,app will read joinaddr from join_file)

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # ppx-app
 基于 hashicorp/raft 一致性去中心化逻辑框架
 
+gen raft code from proto
+```
 go install git.shiyou.kingsoft.com/infra/go-raft/cmd/protoc-gen-go-raft
 protoc *.proto  --go_out ./ --go-grpc_out=./ --go-raft_out=./
+```
--- test/tryjoin_test.go
+++ test/tryjoin_test.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"git.shiyou.kingsoft.com/infra/go-raft/common"
@@ -13,5 +15,20 @@ func Test_tryJoin(t *testing.T) {
 	})
 	clusterAppTemplate(t, func() {
 		t.Logf("Test_tryJoin")
+	})
+}
+
+func Test_JoinFile(t *testing.T) {
+	filename := "cache/join_addr.txt"
+	content := "127.0.0.1:18310\n127.0.0.1:18311"
+	ioutil.WriteFile(filename, []byte(content), os.ModePerm)
+	leaderYaml = genYamlBase(leaderYaml, true, 0, true, func(configure *common.Configure) {
+		configure.JoinFile = ""
+	})
+	followerYaml = genYamlBase(followerYaml, false, 1, true, func(configure *common.Configure) {
+		configure.JoinFile = filename
+	})
+	clusterAppTemplate(t, func() {
+		t.Logf("Test_JoinFile Success")
 	})
 }
